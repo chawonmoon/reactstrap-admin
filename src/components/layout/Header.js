8177349f4e3afb39795beff3058845159417ed8b
wonmooncha <wmcha@scgs.co.kr>
2019-04-15 15:38:01 +0900

    헤더 관리자 레이어팝업창 추가

--- src/components/layout/Header.js
+++ src/components/layout/Header.js
@@ -4,6 +4,24 @@ import { Button } from "reactstrap";
 //import { HomeLogo } from "images";
 
 class Header extends Component {
+  static defaultProps = {
+    adminOpen: false
+  };
+  state = {
+    adminOpen: this.props.adminOpen
+  };
+  adminHandler = () => {
+    this.setState(state => ({
+      adminOpen: !state.adminOpen
+    }));
+  };
+  sidebarHandler = () => {
+    this.setState(state => ({
+      adminOpen: false
+    }));
+    this.props.onSidebar();
+  };
+
   render() {
     return (
       <header className="admin-header" id="admin-header">
@@ -15,14 +33,26 @@ class Header extends Component {
         <a
           className="btn_snb"
           href="javascript:void(0);"
-          onClick={this.props.onSidebar}
+          onClick={this.sidebarHandler}
         >
           <i className="ico-menu" />
         </a>
-        <Button className="btn_admin" color="pink" size="sm">
-          <i className="ico-user-tie" />
-          <span>관리자</span>
-        </Button>
+        <div className={"btn_admin" + (this.state.adminOpen ? " active" : "")}>
+          <Button color="pink" size="sm" onClick={this.adminHandler}>
+            <i className="ico-user-tie" />
+            <span>관리자</span>
+          </Button>
+          <div className="btn_admin_popup">
+            <Button color="pink" size="sm">
+              <i className="ico-profile" />
+              <span>프로필</span>
+            </Button>
+            <Button color="pink" size="sm">
+              <i className="ico-exit" />
+              <span>로그아웃</span>
+            </Button>
+          </div>
+        </div>
       </header>
     );
   }
